45e058621454aadc2a027e04643d943154ce3c3f
ssunhong <bebo007@naver.com>
2025-10-12 20:14:57 +0900

    Initial commit

---
+++ src/main/java/com/ssun/strikenoteserver/StrikenoteServerApplication.java
@@ -0,0 +1,13 @@
+package com.ssun.strikenoteserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StrikenoteServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StrikenoteServerApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ssun/strikenoteserver/StrikenoteServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssun.strikenoteserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StrikenoteServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
